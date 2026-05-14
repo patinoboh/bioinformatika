7b039de7c43fd2a2b86af73bd22a7fb2f6bc2849
patinoboh <patrik.brocek@gmail.com>
2026-05-14 23:50:05 +0200

    asd

--- project/project.py
+++ project/project.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
+from data_profiling import ProfileReport
+
 
 import load_data
 
@@ -14,12 +16,17 @@ def describe_data(data):
     # TODO
     return
 
-def data_report(data, report_path="report/autoreport.pdf"):
+def data_report(data, report_path="reports/autoreport.html"):
+    # profile = ProfileReport(data, title="Data Report", explorative=True).to_file(report_path)
+    profile = ProfileReport(data, title="Data Report").to_file(report_path)
 
+    profile.to_file(report_path)
+    # profile.to_notebook_iframe()
     return report_path
 
 def main(args):
     data = load_data.get_matrix_data(args.input)
+    data_report(data)
 
 
 
